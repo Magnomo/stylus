96f3b166cb5373d5ceb1e9e576724f52244d27be
Diego <diegommorais12@gmail.com>
2020-01-06 20:06:01 -0300

    ajustes na pagina inicial

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -7,5 +7,111 @@
         <p class="lead">Conheça o melhor motel do litoral norte!</p>
     </div>
 </div>
+<div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+    <ol class="carousel-indicators">
+        <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+        <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+        <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+    </ol>
+    <div class="carousel-item active">
+        <img class="d-block w-100" src="{{asset('imgs/um.jpg')}}" alt="First slide">
+    </div>
+    <div class="carousel-item">
+        <img class="d-block w-100" src="{{asset('imgs/dois.jpg')}}" alt="Second slide">
+    </div>
+    <div class="carousel-item">
+        <img class="d-block w-100" src="{{asset('imgs/um.jpg')}}" alt="Third slide">
+    </div>
+</div>
+<a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+</a>
+<a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+</a>
+<div class="box">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-4 col-md-4 col-sm-4 col-xs-12">
+                <div class="box-part text-center">
+                    <i class="fas fa-camera fa-3x" aria-hidden="true"></i>
+                    <div class="title">
+                        <h4>Galery</h4>
+                    </div>
+                    <div class="text">
+                        <span>Lorem ipsum dolor sit amet, id quo eruditi eloquentiam. Assum decore te sed. Elitr scripta ocurreret qui ad.</span>
+                    </div>
+                    <a href="#">Learn More</a>
+                </div>
+            </div>
+            <div class="col-lg-4 col-md-4 col-sm-4 col-xs-12">
+                <div class="box-part text-center">
+                <i class="fas fa-location-arrow fa-3x" aria-hidden="true"></i>
+                
+                    
+                    <div class="title">
+                        <h4>Como chegar</h4>
+                    </div>
+                    <div class="text">
+                        <span>Lorem ipsum dolor sit amet, id quo eruditi eloquentiam. Assum decore te sed. Elitr scripta ocurreret qui ad.</span>
+                    </div>
+                    <a href="#">Learn More</a>
+                </div>
+            </div>
+            <div class="col-lg-4 col-md-4 col-sm-4 col-xs-12">
+                <div class="box-part text-center">
+                    <i class="fas fa-hotel fa-3x" aria-hidden="true"></i>
+                    <div class="title">
+                        <h4>Produtos & Serviços</h4>
+                    </div>
+                    <div class="text">
+                        <span>Lorem ipsum dolor sit amet, id quo eruditi eloquentiam. Assum decore te sed. Elitr scripta ocurreret qui ad.</span>
+                    </div>
+                    <a href="#">Learn More</a>
+                </div>
+            </div>
+            <div class="col-lg-4 col-md-4 col-sm-4 col-xs-12">
+                <div class="box-part text-center">
+                    <i class="fas fa-tags fa-3x" aria-hidden="true"></i>
+                    <div class="title">
+                        <h4>Preços e promoções</h4>
+                    </div>
+                    <div class="text">
+                        <span>Lorem ipsum dolor sit amet, id quo eruditi eloquentiam. Assum decore te sed. Elitr scripta ocurreret qui ad.</span>
+                    </div>
+                    <a href="#">Learn More</a>
+                </div>
+            </div>
+            <div class="col-lg-4 col-md-4 col-sm-4 col-xs-12">
+                <div class="box-part text-center">
+                    <i class="fas fa-comments fa-3x" aria-hidden="true"></i>
+                    <div class="title">
+                        <h4>Fale conosco</h4>
+                    </div>
+                    <div class="text">
+                        <span>Lorem ipsum dolor sit amet, id quo eruditi eloquentiam. Assum decore te sed. Elitr scripta ocurreret qui ad.</span>
+                    </div>
+                    <a href="#">Learn More</a>
+                </div>
+            </div>
+            <div class="col-lg-4 col-md-4 col-sm-4 col-xs-12">
+                <div class="box-part text-center">
+                    <i class="fab fa-facebook fa-3x" aria-hidden="true"></i>
+                    <div class="title">
+                        <h4>Redes sociais</h4>
+                    </div>
+                    <div class="text">
+                        <span>Lorem ipsum dolor sit amet, id quo eruditi eloquentiam. Assum decore te sed. Elitr scripta ocurreret qui ad.</span>
+                    </div>
+                    <a href="#">Learn More</a>
+                </div>
+            </div>
+
+        </div>
+    </div>
+</div>
+
 <link rel="stylesheet" type="text/css" href="{{asset('css/home.css')}}">
 @endsection
--- resources/views/layouts/template.blade.php
+++ resources/views/layouts/template.blade.php
@@ -12,6 +12,7 @@
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+
     <title>Document</title>
 </head>
 
@@ -52,6 +53,93 @@
             <div class="col-md-12 h-100">
                 @yield('body')
             </div>
+            <footer class="footer_area w-100">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-md-3 col-sm-6">
+                            <div class="single_ftr">
+                                <h4 class="sf_title">Contacts</h4>
+                                <ul>
+                                    <li>Av. José Herculano, 8600</li>
+                                    <li>12 3887-9479 <br> (+055) 3896-2086</li>
+                                    <li>contato@motelstylus.com</li>
+                                </ul>
+                            </div>
+                        </div> <!--  End Col -->
+
+                        <div class="col-md-3 col-sm-6">
+                            <div class="single_ftr">
+                                <h4 class="sf_title">Informações</h4>
+                                <ul>
+                                    <li><a href="#">Sobre nós </a></li>
+                                    <li><a href="#">Termos & Condições</a></li>
+                                    <li><a href="#">Fale conosco</a></li>
+                                </ul>
+                            </div>
+                        </div> <!--  End Col -->
+
+                        <div class="col-md-3 col-sm-6">
+                            <div class="single_ftr">
+                                <h4 class="sf_title">Serviços</h4>
+                                <ul>
+                                    <li><a href="#">check-in and check-out</a></li>
+                                    <li><a href="#">Higiene</a></li>
+                                    <li><a href="#">Segurança 24 horas</a></li>
+                                    <li><a href="#">Restaurante</a></li>
+                                </ul>
+                            </div>
+                        </div> <!--  End Col -->
+
+                        <div class="col-md-3 col-sm-6">
+                            <div class="single_ftr">
+                                <h4 class="sf_title">Noticias</h4>
+                                <div class="newsletter_form">
+                                    <p>Informe seu e-mail para receber noticias sobre promoções e sobre nossos serviços </p>
+                                    <form method="post" class="form-inline">
+                                        <input name="EMAIL" id="email" placeholder="Enter Your Email" class="form-control" type="email">
+                                        <button type="submit" class="btn btn-default"><i class="fa fa-search"></i></button>
+                                    </form>
+                                </div>
+                            </div>
+                        </div> <!--  End Col -->
+
+                    </div>
+                </div>
+
+
+                <div class="ftr_btm_area">
+                    <div class="container">
+                        <div class="row">
+                            <div class="col-sm-4">
+                                <div class="ftr_social_icon">
+                                    <ul>
+                                        <li><a href="#"><i class="fab fa-facebook"></i></a></li>
+                                        <li><a href="#"><i class="fab fa-google"></i></a></li>
+                                        <li><a href="#"><i class="fab fa-linkedin"></i></a></li>
+                                        <li><a href="#"><i class="fab fa-twitter"></i></a></li>
+                                        <li><a href="#"><i class="fa fa-rss"></i></a></li>
+                                    </ul>
+                                </div>
+                            </div>
+                            <div class="col-sm-4">
+                                <p class="copyright_text text-center">© 2019 All Rights Reserved</p>
+                            </div>
+
+                            <div class="col-sm-4">
+                                <div class="payment_mthd_icon text-right">
+                                    <ul>
+                                        <li><i class="fab fa-cc-paypal"></i></li>
+                                        <li><i class="fab fa-cc-visa"></i></li>
+                                        <li><i class="fab fa-cc-discover"></i></li>
+                                        <li><i class="fab fa-cc-mastercard"></i></li>
+                                        <li><i class="fab fa-cc-amex"></i></li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </footer>
         </div>
 
     </div>
